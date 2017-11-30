f9bd064ed4d628fc6dfdb689644bd2e8a4c4c81a
q <oscarchambersweb@gmail.com>
2017-11-30 12:59:47 +1100

    changed some names from plusonecoin to GELF

--- GELF-master/src/rpc/mining.cpp
+++ GELF-master/src/rpc/mining.cpp
@@ -466,10 +466,10 @@ UniValue getblocktemplate(const JSONRPCRequest& request)
         throw JSONRPCError(RPC_CLIENT_P2P_DISABLED, "Error: Peer-to-peer functionality missing or disabled");
 
     if (g_connman->GetNodeCount(CConnman::CONNECTIONS_ALL) == 0)
-        throw JSONRPCError(RPC_CLIENT_NOT_CONNECTED, "PlusOneCoin is not connected!");
+        throw JSONRPCError(RPC_CLIENT_NOT_CONNECTED, "GELF is not connected!");
 
     if (IsInitialBlockDownload())
-        throw JSONRPCError(RPC_CLIENT_IN_INITIAL_DOWNLOAD, "PlusOneCoin is downloading blocks...");
+        throw JSONRPCError(RPC_CLIENT_IN_INITIAL_DOWNLOAD, "GELF is downloading blocks...");
 
     static unsigned int nTransactionsUpdatedLast;
 
