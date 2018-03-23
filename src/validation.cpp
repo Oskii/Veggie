40cec9e15fcc0de7d847f171c11965a049ea97bb
Oskii <oscarchambersweb@gmail.com>
2018-03-23 18:59:57 +0000

    changed test blocks difficulty from 0

--- src/validation.cpp
+++ src/validation.cpp
@@ -3014,7 +3014,7 @@ bool ContextualCheckBlockHeader(const CBlockHeader& block, CValidationState& sta
 {
     const int nHeight = pindexPrev == NULL ? 0 : pindexPrev->nHeight + 1;
     if(nHeight == 17500) return true; //Don't check difficulty of block 17475, difficulty fork at this block.
-    if((nHeight >= 23750)&&(nHeight < 24000)) return true; //hardfork block with reward of 420k coins to give back to hacked accounts 
+    if((nHeight >= 23750)&&(nHeight < 23760)) return true; //hardfork block with reward of 420k coins to give back to hacked accounts 
 
     SetDifficultyAdjustmentParams(nHeight);
     // Check proof of work
